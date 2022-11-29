38af96f1ee60b2c896d5bc1d4f8ac209daad9ba7
Ali Abbiss <aliabbiss5@gmail.com>
2022-11-28 21:24:15 -0500

    Updated DAL, added TrainerController with CRUD ops working

---
+++ Pokemon/Controllers/TrainerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pokemon.Controllers
+{
+    [Route("trainer")]
+    [ApiController]
+    public class TrainerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Trainer> GetAllTrainers()
+        {
+            return DAL.GetAllTrainers();
+        }
+
+        [HttpGet("{id}")]
+        public Trainer GetTrainer(int id)
+        {
+            return DAL.GetTrainer(id);
+        }
+
+        [HttpPost]
+        public Trainer AddTrainer(Trainer trainer)
+        {
+            return DAL.AddTrainer(trainer);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteTrainer(int id)
+        {
+            DAL.Delete(id);
+        }
+
+        [HttpPut]
+        public void Update(Trainer trainer)
+        {
+            DAL.Update(trainer);
+        }
+    }
+}
--- Pokemon/Models/DAL.cs
+++ Pokemon/Models/DAL.cs
@@ -1,6 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using Dapper.Contrib.Extensions;
-
+using System.Diagnostics.Metrics;
 
 namespace Pokemon
 {
@@ -8,6 +8,33 @@ namespace Pokemon
     {
         // hola
         public static MySqlConnection DB;
+        // Do we want to do this the new way Jeff showed us during Flex week? E.g... opening and closing
+        // the MySqlConnection properly each time we use it to avoid those annoying errors.
+
+        // So, 
+        // "public static string CS"  **CS stands for Connection String and lives in DAL.cs**
+        // Instead of "public static mySqlConnection DB"
+        // Then, we'd create 2 variables in "program.cs"
+        //      string connstring = app.Configuration.GetConnectionString("db");
+        //      DAL.CS = connstring;
+
+        // Example below of referencing connection string new way 
+        /*
+         
+        public static List<ShortRepairList> getAll()
+        {
+            // Here's how we do it with just a query and not a view
+            //return DAL.DB.Query<ShortRepairOrder>("select ro.id, ro.customer, ins.name as Instrument from repairorder ro join instrument ins on ro.instrument_id = ins.id;").ToList();
+            // But some argue that putting queries in C# code is not the best idea.
+            MySqlConnection db = new MySqlConnection(DAL.CS);
+            db.Open();
+            var result = db.GetAll<ShortRepairList>().ToList();
+            db.Close();
+            return result;
+        } 
+
+         */
+
 
         public static IEnumerable<Team> GetAllTeams()  //DB Team
         {
@@ -29,5 +56,38 @@ namespace Pokemon
         {
             DB.Update(team);
         }
+
+
+
+
+        // TRAINER Static CRUD Operations (OLD WAY)
+
+        public static IEnumerable<Trainer> GetAllTrainers()
+        {
+            return DB.GetAll<Trainer>();
+        }
+
+        public static Trainer GetTrainer(int id)
+        {
+            return DB.Get<Trainer>(id);
+        }
+
+        public static Trainer AddTrainer(Trainer trainer)
+        {
+            DB.Insert(trainer);
+            return trainer;
+        }
+
+        public static void Update(Trainer trainer)
+        {
+            DB.Update(trainer);
+        }
+
+        public static void Delete(int id)
+        {
+            DB.Delete(new Trainer { id = id });
+        }
+
+
     }
 }
