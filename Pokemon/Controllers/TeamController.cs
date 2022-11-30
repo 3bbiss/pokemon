99fcb94be0c54b4b3d02f2cccfa1d94466b9bea7
Brynn-Hamilton <Brynn.s.hamilton@gmail.com>
2022-11-30 14:33:20 -0500

    Updated GetAllPokemon & pokemomcontroller

--- Pokemon/Controllers/TeamController.cs
+++ Pokemon/Controllers/TeamController.cs
@@ -5,29 +5,49 @@ using Dapper.Contrib.Extensions;
 
 namespace Pokemon.Controllers
 {
-    [Route("team")]
+    
     [ApiController]
     public class TeamController : ControllerBase
     {
+        
         [HttpGet]
+        [Route("team")]
         public async Task<IEnumerable<TeamDisplay>> GetAll()
         {
             return await TeamDisplay.GetAllTeams();
         }
 
+        [HttpGet]
+        [Route("team/{id}")]
+        public async Task<TeamDisplay> Get(int team_id)
+        {
+            return await TeamDisplay.GetOneTeam(team_id);
+        }
+
+        [HttpGet]
+        [Route("team/trainer/{id}")]
+        public async Task<IEnumerable<TeamDisplay>> GetAllTrainerTeams(int trainer_id)
+        {
+            return await TeamDisplay.GetAllTeamsbyTrainer(trainer_id);
+        }
+
         [HttpPost]
+        [Route("team")]
         public void Add(TeamDisplay team)
         {
             TeamDisplay.AddTeam(team);
         }
 
+        [HttpDelete]
         [HttpDelete("{id}")]
+        [Route("team/{id}")]
         public void Delete(int id)
         {
             TeamDisplay.DeleteTeam(id);
         }
        
         [HttpPut]
+        [Route("team")]
         public void Update(TeamDisplay team)
         {
             TeamDisplay.UpdateTeam(team);
--- Pokemon/Models/Pokemon.cs
+++ Pokemon/Models/Pokemon.cs
@@ -8,21 +8,14 @@ namespace Pokemon
         public int id { get; set; }
         public string name { get; set; }
         public int height { get; set; }
-        public bool is_default { get; set; } // Do we want this for a default image/version?
-        // could be used for just the initial image display and then have a conditional for when we don't want it
-
-        public int order { get; set; } // idk what this is
+        public bool is_default { get; set; } 
+        public int order { get; set; } 
         public int weight { get; set; }
         public List<Move> moves { get; set; }
         public PokemonSprites sprites { get; set; }
-
-        // may be nested - separate from species call in pokemon resource
         public SpeciesName species { get; set; }
         public List<Stats> stats { get; set; }
         public List<Type> types { get; set; }
-
-
-
 
         public static async Task<Pokemon> GetPokemon(int id)
         {
@@ -33,16 +26,16 @@ namespace Pokemon
             return response;
         }
 
+        // busted - issue with how data returns from api when doing limit
+        // is different from the pokemon object for get one
+        //public static async Task<List<Pokemon>> GetAllPokemon()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    var pokeRequest = await client.GetAsync($"https://pokeapi.co/api/v2/pokemon/?limit=150");
+        //    List<Pokemon> response = await pokeRequest.Content.ReadAsAsync<List<Pokemon>>();
 
-
-        public static async Task<List<Pokemon>> GetAllPokemon()
-        {
-            HttpClient client = new HttpClient();
-            var pokeRequest = await client.GetAsync($"https://pokeapi.co/api/v2/pokemon/?limit=150");
-            List<Pokemon> response = await pokeRequest.Content.ReadAsAsync<List<Pokemon>>();
-
-            return response;
-        }
+        //    return response;
+        //}
 
 
     }
