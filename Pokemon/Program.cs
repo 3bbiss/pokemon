7377cf7333b2865fac5a648898139c774e14da8c
Ali Abbiss <aliabbiss5@gmail.com>
2022-11-30 16:49:18 -0500

    updated program.cs to prep for angular stuffs

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -8,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "LocalOriginsPolicy",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+    );
+}
+);
 
 var app = builder.Build();
 
@@ -17,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("LocalOriginsPolicy");
 
 app.UseHttpsRedirection();
 
